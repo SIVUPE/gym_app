7dc2e586b2c17e9d1b405af34c649a9f6e3bb0f5
SIVUPE <130312850+SIVUPE@users.noreply.github.com>
2024-02-22 12:47:27 +0200

    Delete backend directory

--- backend/model/index.js
+++
@@ -1,9 +0,0 @@
-import { Users } from "./Users.js";
-import { Products } from "./Products.js";
-// Create objects
-let users = new Users()
-let products = new Products()
-export{
-    users,
-    products
-}  

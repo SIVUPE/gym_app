c31919776b7b751f15037771801635e3f0213e55
tyhieshajohnson <johnsontyhiesha@gmail.com>
2024-02-23 15:43:54 +0200

    Services Page WIP

---
+++ frontend/frontend-ty/src/components/Merch.vue
@@ -0,0 +1,22 @@
+<!-- HTML -->
+<template>
+    <!-- Products Called Here -->
+</template>
+  
+  <!-- JAVASCRIPT -->
+  <script>
+  export default {
+    name: 'Footer',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- CSS -->
+  <style scoped>
+  
+  @media only screen and (max-width: 600px) {
+
+  }
+  </style>
--- frontend/frontend-ty/src/views/ServiceView.vue
+++ frontend/frontend-ty/src/views/ServiceView.vue
@@ -1,5 +1,69 @@
+<!-- HTML -->
 <template>
-    <div class="services">
-      <h1>This is an services page</h1>
+  <div class="services" style="display: flex">
+    <div>
+      <h1 style="color: #f9ef23">WHAT WE OFFER</h1>
+      <p>We're committed to bringing you the best workout experience.</p>
     </div>
-  </template>
+    <!-- IMAGES -->
+    <div class="image-content">
+      <!-- image 1 -->
+      <div class="home-content">
+        <img
+          src="https://i.ibb.co/8jSDb8r/Non-Zero-Gravity-Activewear.jpg"
+          style="width: 100px; height: 100px"
+          alt=""
+        />
+        <p class="text-container">MEMBERSHIPS</p>
+      </div>
+
+      <!-- image 2 -->
+      <div class="home-content">
+        <img
+          src="https://i.ibb.co/7p70XFd/Gym-Background.jpg"
+          style="width: 100px; height: 100px"
+          alt=""
+        />
+        <p class="text-container">CLASSES</p>
+      </div>
+
+      <!-- image 3 -->
+      <div class="home-content">
+        <img
+          src="https://i.ibb.co/pJwcG63/Magma-Sports-wear-Gym-Editorial-Shoot-6.jpg"
+          style="width: 100px; height: 100px"
+          alt=""
+        />
+        <p class="text-container">MERCH</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- JAVASCRIPT -->
+<script></script>
+
+<!-- CSS -->
+<style scoped>
+.services {
+  background-color: #141414;
+}
+
+.image-content {
+  display: flex;
+  justify-content: space-between;
+}
+
+.home-content {
+  position: relative;
+}
+
+.text-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+}
+</style>

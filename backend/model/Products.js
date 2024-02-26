370016e21d4b80ef0a13488910435a1ac93fba8b
SIVUPE <sivuyisiwepeter79@gmail.com>
2024-02-26 11:45:07 +0200

    updates

---
+++ backend/model/Products.js
@@ -0,0 +1,84 @@
+import { connection as db } from "../config/index.js";
+
+class Products {
+    fetchUsers(req, res) {
+      const qry = `
+          SELECT prodID,
+           prodName,
+          prodQuantity,
+          prodAmount,
+          userID,
+          FROM Products;
+          `;
+      db.query(qry, [req.body],(err, results) => {
+        if (err) throw err;
+        res.json({
+          status: res.statusCode,
+          results,
+        })
+      })
+    }
+    fetchUser(req, res) {
+        const qry = `
+        SELECT prodID, 
+        prodName,
+        prodQuantity,
+    
+         prodAmount,
+        userID
+        FROM Users;
+        WHERE userID = ${req.params.id};
+        `;
+        db.query(qry, (err, result) => {
+          if (err) throw err;
+          res.json({
+            status: res.statusCode,
+            result: result[0]
+          })
+        })
+      }
+      addProducts(req, res) {
+        const qry = `
+        INSERT INTO Products 
+        SET ?;
+        `
+        db.query(qry,(err)=>{
+            if(err) throw err 
+            res.json({
+                status: res.statusCode,
+                msg: 'New product was added'
+            })
+        })
+      }
+      updateProduct(req, res) {
+        const qry = `
+        UPDATE Products
+        SET ?
+        WHERE prodID = ${req.params.id};
+        `
+        db.query(qry, [req.body], (err)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode, 
+                msg: "The product information has been updated."
+            })
+        })
+    }
+    deleteProduct(req, res) {
+        const qry = `
+        DELETE FROM Products
+        WHERE prodID = ${req.params.id};
+        `
+        db.query(qry, (err)=>{
+            if(err) throw err 
+            res.json({
+                status: res.statusCode, 
+                msg: "The product information has been deleted."
+            })
+        })
+    }
+
+}
+export{
+    Products
+}
